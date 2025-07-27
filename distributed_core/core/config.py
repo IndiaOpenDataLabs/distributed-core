a0bb9101620f4146eb22036011006972a5c157bd
Madhav Kandukuri <madhav165@gmail.com>
2025-07-27 22:45:00 +0530

    lint fix

--- distributed_core/core/config.py
+++ distributed_core/core/config.py
@@ -1,7 +1,9 @@
 """
 # distributed_core/core/config.py
 """
-from pydantic import BaseSettings
+
+from pydantic_settings import BaseSettings
+
 
 class Settings(BaseSettings):
     """
@@ -12,5 +14,6 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
+
 
 settings = Settings()
